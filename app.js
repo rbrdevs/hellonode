f7b5e2b7651648241dd40440ed71f642cf3088b1
vagrant <vagrant@arsnova-dev>
2014-08-02 22:02:14 +0000

    fix

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var s = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 });
-s.listen(8000);
+s.listen(3000);
 console.log('Server running!');
